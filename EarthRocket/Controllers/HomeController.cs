05a71360dd959a494cf3c1d83c056fc67dffd0f0
Jasun <jasun.feddema@gmail.com>
2018-04-24 17:09:31 -0700

    everything

---
+++ EarthRocket/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace EarthRocket.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Dictionary<string, float> model = new Dictionary<string, float>();
+            double lon = -31.2357 * Math.PI / 180;
+            double lat = 30.0444 * Math.PI / 180;
+            double a = Math.Cos(lat) * 1.25;
+            double y = Math.Sin(lat) * 1.25;
+            double x = Math.Cos(lon) * a;
+            double z = Math.Sin(lon) * a;
+
+            model["x"] = (float)x;
+            model["y"] = (float)y;
+            model["z"] = (float)z;
+            return View(model);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ EarthRocket/Models/Coordinate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EarthRocket.Models
+{
+    public class Coordinate
+    {
+        private int DegreeLat;
+        private int MinuteLat;
+        private int DegreeLon;
+        private int MinuteLon;
+
+        public Coordinate(int dLat, int mLat, int dLon, int mLon)
+        {
+            this.DegreeLat = dLat;
+            this.MinuteLat = mLat;
+            this.DegreeLon = dLon;
+            this.MinuteLon = mLon;
+        }
+
+        public static int GetDistance(Coordinate a, Coordinate b)
+        {
+            float aLatDegrees = a.DegreeLat + a.MinuteLat / 60f;
+            float aLonDegrees = a.DegreeLon + a.MinuteLon / 60f;
+
+            float bLatDegrees = b.DegreeLat + b.MinuteLat / 60f;
+            float bLonDegrees = b.DegreeLon + b.MinuteLon / 60f;
+
+            float distLat = aLatDegrees - bLatDegrees;
+            float distLon = aLonDegrees - bLonDegrees;
+
+            float distDegrees = (float)Math.Sqrt(distLat * distLat + distLon * distLon);
+
+            return (int)Math.Round(distDegrees * 60);
+        }
+    }
+}
